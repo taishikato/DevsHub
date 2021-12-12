0c7d43e68a0eff953cec68b3d1557c7d48152de8
taishikato <taishi.k0903@gmail.com>
2021-12-12 23:19:20 +0900

    Update Login page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className="p-3 text-center">
-      <Link to="/">Title</Link>
+      <Link to="/" className="font-bold">
+        DevsHub
+      </Link>
     </div>
   )
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,5 @@
 import Header from './Header'
+import { IoLogoGithub } from 'react-icons/io5'
 
 const Login = () => {
   const handleLogin = () => {
@@ -6,16 +7,19 @@ const Login = () => {
   }
 
   return (
-    <>
+    <div className="h-screen">
       <Header />
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center h-full">
+        <p className="mb-5 text-lg font-semibold">Welcome! Let's get started your journey!</p>
         <div
           onClick={handleLogin}
-          className="px-4 py-2 font-semibold text-gray-800 bg-white border border-gray-300 rounded-md shadow cursor-pointer hover:bg-gray-100">
+          className="flex items-center px-4 py-2 font-semibold text-gray-800 bg-white border border-gray-300 rounded-md shadow cursor-pointer hover:bg-gray-100"
+        >
+          <IoLogoGithub className="mr-2" size={23} />
           Login with GitHub
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
